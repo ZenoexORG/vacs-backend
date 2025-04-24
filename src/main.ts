5d89ddfb11c34e616a71f80e38f8049d419bfaeb
XNeyMo <xneymodev@protonmail.com>
2025-04-24 18:54:17 -0500

    feat(main.ts): cors added

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    credentials: true,
+  })
+
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, document);
+
   app.use(cookieParser(process.env.COOKIE_SECRET || 'super_safe_secret'));
   app.useGlobalInterceptors(new LocalToUTCInterceptor(), new UTCToLocalInterceptor());
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
