a84927fa2c61274c6bad5227ed670042eeab6b0b
XNeyMo <xneymodev@protonmail.com>
2025-04-27 23:06:29 -0500

    feat(permissions.enum.ts): names update

--- src/shared/enums/permissions.enum.ts
+++ src/shared/enums/permissions.enum.ts
@@ -1,49 +1,49 @@
-export enum AppPermissions {    
-    EMPLOYEE_CREATE = 'employees:write',
-    EMPLOYEE_READ = 'employees:read',
+export enum AppPermissions {
+    EMPLOYEE_CREATE = 'employees:create',
+    EMPLOYEE_VIEW = 'employees:view',
     EMPLOYEE_UPDATE = 'employees:edit',
     EMPLOYEE_DELETE = 'employees:delete',
-        
-    ROLE_CREATE = 'roles:write',
-    ROLE_READ = 'roles:read',
+
+    ROLE_CREATE = 'roles:create',
+    ROLE_VIEW = 'roles:view',
     ROLE_UPDATE = 'roles:edit',
-    ROLE_DELETE = 'roles:delete',    
-        
-    VEHICLE_CREATE = 'vehicles:write',
-    VEHICLE_READ = 'vehicles:read',
+    ROLE_DELETE = 'roles:delete',
+
+    VEHICLE_CREATE = 'vehicles:create',
+    VEHICLE_VIEW = 'vehicles:view',
     VEHICLE_UPDATE = 'vehicles:edit',
     VEHICLE_DELETE = 'vehicles:delete',
-    
-    VEHICLE_TYPES_CREATE = 'vehicle-types:write',
-    VEHICLE_TYPES_READ = 'vehicle-types:read',
+
+    VEHICLE_TYPES_CREATE = 'vehicle-types:create',
+    VEHICLE_TYPES_VIEW = 'vehicle-types:view',
     VEHICLE_TYPES_UPDATE = 'vehicle-types:edit',
     VEHICLE_TYPES_DELETE = 'vehicle-types:delete',
-        
-    USER_CREATE = 'users:write',
-    USER_READ = 'users:read',
+
+    USER_CREATE = 'users:create',
+    USER_VIEW = 'users:view',
     USER_UPDATE = 'users:edit',
     USER_DELETE = 'users:delete',
-        
-    PERMISSION_CREATE = 'permissions:write',
-    PERMISSION_READ = 'permissions:read',
+
+    PERMISSION_CREATE = 'permissions:create',
+    PERMISSION_VIEW = 'permissions:view',
     PERMISSION_UPDATE = 'permissions:edit',
     PERMISSION_DELETE = 'permissions:delete',
     PERMISSION_ASSIGN = 'permissions:assign',
     PERMISSION_UNASSIGN = 'permissions:unassign',
 
-    ACCESS_LOG_CREATE = 'access-logs:write',
-    ACCESS_LOG_READ = 'access-logs:read',
+    ACCESS_LOG_CREATE = 'access-logs:create',
+    ACCESS_LOG_VIEW = 'access-logs:view',
     ACCESS_LOG_UPDATE = 'access-logs:edit',
     ACCESS_LOG_DELETE = 'access-logs:delete',
 
-    INCIDENTS_CREATE = 'incidents:write',
-    INCIDENTS_READ = 'incidents:read',
+    INCIDENTS_CREATE = 'incidents:create',
+    INCIDENTS_VIEW = 'incidents:view',
     INCIDENTS_UPDATE = 'incidents:edit',
-    INCIDENTS_DELETE = 'incidents:delete',    
+    INCIDENTS_DELETE = 'incidents:delete',
 
-    DASHBOARD_READ = 'dashboard:read',
-    
+    DASHBOARD_VIEW = 'dashboard:view',
+
     REPORTS_GENERATE = 'daily-reports:generate',
-    REPORTS_READ = 'daily-reports:read',
+    REPORTS_VIEW = 'daily-reports:view',
     REPORTS_DELETE = 'daily-reports:delete',
 }
